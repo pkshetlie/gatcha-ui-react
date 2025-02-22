e0364e28e48bf3c20e97faf600cd5ffcb429c521
Rhokapa <pierrick.pobelle@gmail.com>
2025-02-22 20:43:21 +0100

    Add environment-based config setup for URLs
    Introduced a `.env` file to manage URL configurations, improving flexibility for different environments. Created a `config.js` to centralize environment variables, and updated `.gitignore` to exclude it to ensure sensitive data is not tracked.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 const config = {
-    BASE_URL: process.env.REACT_APP_BASE_URL,
-    IMAGE_BASE_URL: process.env.REACT_APP_IMAGE_BASE_URL,
-    INVENTORY_IMAGE_BASE_URL: process.env.REACT_APP_INVENTORY_IMAGE_BASE_URL,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL
+    BASE_URL: BASE_URL,
+    IMAGE_BASE_URL: `${BASE_URL}`,
+    INVENTORY_IMAGE_BASE_URL: `${BASE_URL}imgs/inventory`,
+    API_BASE_URL: `${BASE_URL}api`
 };
 
 export default config;
